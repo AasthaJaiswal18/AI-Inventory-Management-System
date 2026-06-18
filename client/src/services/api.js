2b2b52b7d0dda198d510a941fb45f0f14df8a60c
AasthaJaiswal18 <ak0980346@gmail.com>
2026-06-18 16:41:34 +0530

    Updated frontend API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-inventory-management-system-1di5.onrender.com/api",
 });
 
 export default API;
